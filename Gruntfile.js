3b7c1012c8563466d7b5497baaa1ffee1fd14470
Henry Charge <henry.charge@valtech.co.uk>
2016-07-20 09:20:18 +0100

    Added license headers to files and copied over admin files from Fast Track

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2016 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
 module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
@@ -312,7 +328,7 @@ module.exports = function(grunt) {
               replace: true
             },
             files: {
-              src: [ 'www/_assets/js/prototype.js' ]
+              src: [ 'Gruntfile.js', 'www/_assets/css/main-ie8.css', 'www/_assets/css/main.css', 'www/_assets/js/**/*.js', '!www/_assets/js/vendor/' ]
             }
           }
         }
